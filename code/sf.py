6d08483e3c41860d6424aa94accdf447e8c31420
evansj <evansj@email.chop.edu>
2015-08-12 21:30:36 -0400

    Add snakefile.

---
+++ code/sf.py
@@ -0,0 +1,17 @@
+STATIC = '../pdf/'
+
+rule cv:
+    input:   'cv.tex',
+             'publications.bib'
+    output:  'cv.pdf'
+    run:
+        prefix = list(input)[0].split('.')[0]
+        pdfCmd = 'pdflatex -output-directory=%s -output-format=pdf %s' % (STATIC, prefix)
+        latexCmd = 'latex %s' % (prefix, )
+        bibtexCmd = 'bibtex %s' % (prefix, )
+        
+        shell(latexCmd)
+        shell(bibtexCmd)
+        shell(latexCmd)
+        shell(latexCmd)
+        shell(pdfCmd)
